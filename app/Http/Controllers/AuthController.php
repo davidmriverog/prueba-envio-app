b79cd3829304752f792f3e7576d433deb94211c8
David Rivero (ErCompa) <davidmriverog@gmail.com>
2018-12-05 23:57:19 -0300

    service de seguridad

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Laravel\Passport\Client;
+use Illuminate\Support\Facades\Route;
+
 class AuthController extends Controller
 {
-    //
+    public function sendAuthorization(Request $request)
+    {
+        $client = Client::find(2);
+
+        $request->request->add([
+            'grant_type' => 'password',
+            'username' => $request->username,
+            'password' => $request->password,
+            'client_id' => $client->id,
+            'client_secret' => $client->secret,
+            'scope' => '*'
+        ]);
+
+        $proxy = Request::create(
+            'oauth/token',
+            'POST'
+        );
+
+        $data = json_decode(Route::dispatch($proxy)->getContent(), TRUE);
+
+        if(isset($data['error']))
+            return response()->json([
+                'status' => 'failed',
+                'message' => ['Invalid Credentials']
+            ]);
+
+        $data['status'] = 'success';
+
+        return response()->json($data);
+    }
 }
